78c105070548db0e22bf2adda0be120794defdb4
Maaike <maaikejvp@hotmail.com>
2024-01-30 10:26:17 +0100

    oo

---
+++ OOProject/OOProject/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOProject
+{
+    internal class Car
+    {
+        private string _color;
+        public string Color {
+            get => _color;
+            set => _color = value.ToLower();
+        }
+        public int CurrentSpeed { get; set; }
+
+        public int AantalWielen { get; set; }
+
+        public void StartCar()
+        {
+            Console.WriteLine("Car with color " + Color + " started");
+        }
+
+        public (string, int) GetContentTrunk()
+        {
+            return ("hondje", 2);
+        }
+
+        public void IncreaseSpeed(int speedDelta)
+        {
+            CurrentSpeed += speedDelta; // kort voor: CurrentSpeed = CurrentSpeed + speedDelta;
+        }
+    }
+}
---
+++ OOProject/OOProject/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace OOProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.AantalWielen = 4;
+            car.Color = "Black";
+            Console.WriteLine(car.Color);
+            Console.WriteLine("--------");
+            Car car2 = new Car();
+            car2.AantalWielen = 5;
+            car2.Color = "Red";
+            Console.WriteLine(car2.Color);
+            car.StartCar();
+            (string inhoud, int aantal) = car.GetContentTrunk();
+            Console.WriteLine(inhoud);
+            Console.WriteLine(car.GetContentTrunk());
+            car2.IncreaseSpeed(30);
+            Console.WriteLine(car2.CurrentSpeed);
+            car2.IncreaseSpeed(30);
+            Console.WriteLine(car2.CurrentSpeed);
+
+
+
+        }
+    }
+}
