df7679c8747f4d2992edf75d6266a0a8ba007409
Maaike <maaikejvp@hotmail.com>
2024-02-01 11:50:19 +0100

    exceptions

---
+++ ExceptionsEnzo/ExceptionsEnzo/ComputerSaysNoException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsEnzo
+{
+    internal class ComputerSaysNoException : Exception
+    {
+        // eerste no args
+        public ComputerSaysNoException(): base() { }
+
+        // tweede message
+        public ComputerSaysNoException(string message) : base(message) { }
+
+        // derde message + andere exception 
+        public ComputerSaysNoException(string message, Exception e) : base(message, e)
+        {
+
+        }
+
+    }
+}
---
+++ ExceptionsEnzo/ExceptionsEnzo/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ExceptionsEnzo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ExceptionVoorbeeld();
+            try
+            {
+                CodeThatThrowsException();
+            }
+            catch (ComputerSaysNoException ex) 
+            { 
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void ExceptionVoorbeeld()
+        {
+            int y = 0;
+            try
+            {
+                int x = 100 / y;
+            }
+            catch(ArithmeticException ex)
+            {
+                Console.WriteLine("arithmetic problems: " + ex.ToString());
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+
+            }
+            Console.WriteLine("Rest of the code....");
+
+        }
+
+        static void CodeThatThrowsException()
+        {
+            throw new ComputerSaysNoException();
+        }
+    }
+}
