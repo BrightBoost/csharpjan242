643c91a304d2505c61baa3df81d949887ceb8467
Maaike <maaikejvp@hotmail.com>
2024-02-01 13:42:29 +0100

    oa sheets

---
+++ ExceptionsEnzo/ExceptionsEnzo/BankAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsEnzo
+{
+    public class BankAccount
+    {
+        public BankAccount() { }
+        public string AccountName { get; set; }
+        public decimal Balance { get; set; }
+        public decimal MinBalance { get; set; }
+
+        public void Withdraw(decimal amount)
+        {
+            if(Balance - amount > MinBalance)
+            {
+                Balance = Balance - amount;
+            }
+        }
+
+        public void Deposit(decimal amount)
+        {
+            if(amount < 0)
+            {
+                throw new InvalidDataException();
+            }
+            Balance = Balance + amount;
+        }
+
+    }
+}
---
+++ ExceptionsEnzo/ExceptionsEnzoTests/BankAccountTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExceptionsEnzo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ExceptionsEnzo.Tests
+{
+    [TestClass()]
+    public class BankAccountTests
+    {
+        [TestMethod()]
+        public void DepositTest()
+        {
+            BankAccount account = new BankAccount();
+            account.Deposit(100);
+            Assert.AreEqual(100, account.Balance);
+        }
+
+        [TestMethod()]
+        public void DepositNegativeAmountTest()
+        {
+            BankAccount account = new BankAccount();
+            try
+            {
+                account.Deposit(-100);
+            }
+            catch (InvalidDataException e)
+            {
+                StringAssert.Contains(e.Message, "Found invalid data while decoding");
+                return;
+            }
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void WithdrawTest()
+        {
+            BankAccount account = new BankAccount();
+            account.MinBalance = -100;
+            account.Withdraw(75);
+            Assert.AreEqual(-75, account.Balance);
+        }
+    }
+}
